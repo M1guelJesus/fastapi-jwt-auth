2566694fe015806a74e32d0f92ffbfb1d200b043
Miguel Jesus <miguelangelolealjesus@gmail.com>
2023-09-01 22:02:57 +0100

    fix: version

--- fastapi_jwt_auth/__init__.py
+++ fastapi_jwt_auth/__init__.py
@@ -1,6 +1,6 @@
 """FastAPI extension that provides JWT Auth support (secure, easy to use and lightweight)"""
 
-__version__ = "0.7.1"
+__version__ = "0.7.3"
 
 from .auth_jwt import AuthJWT
 from .auth_jwt import AuthJWTRefresh
